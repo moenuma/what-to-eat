c97bb1d558d7b4f13293946c81ebf9f8128897e8
Moe N <moenumasawa@gmail.com>
2022-05-11 15:52:32 +0900

    refactor structure

---
+++ frontend/src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Search } from './search/Search';
+export { default as Result } from './result/Result';
+export { default as Header } from './header/Header';
+export { default as Footer } from './footer/Footer';
