5f2812cb237a0f5ea02700cd53d79d6dfe45e9d2
Moe N <moenumasawa@gmail.com>
2022-05-12 14:42:22 +0900

    add header

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -3,7 +3,10 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='wte__header'>
+            <h5>Search Engine</h5>
+            <h1>What to Eat</h1>
+        </div>
     )
 }
 
