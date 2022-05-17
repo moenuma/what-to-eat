1690281c5d879b720df6e477e9f4f410baf50260
Moe N <moenumasawa@gmail.com>
2022-05-17 23:08:41 +0900

    make it stateful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,28 @@ import React from 'react'
 import { Search, Result, Header, Footer } from './containers';
 import './App.css'
 
-const App = () => {
-    return (
-        <div className='App'>
-            <Header />
-            <Search />
-            <Result />
-            <Footer />
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                <Header />
+                <Search />
+                <Result />
+                <Footer />
+            </div>
+        )
+    }
 }
+
+// const App = () => {
+//     return (
+//         <div className='App'>
+//             <Header />
+//             <Search />
+//             <Result />
+//             <Footer />
+//         </div>
+//     )
+// }
 
 export default App
--- frontend/src/containers/search/Search.jsx
+++ frontend/src/containers/search/Search.jsx
@@ -1,49 +1,96 @@
 import React from 'react'
 import './search.css'
 
-const Search = () => {
-    return (
-        <form action="">
-            <div className='wte__search'>
-                <div className='wte__search-container'>
-                    <div className='wte__search-content'>
-                        <h4>Location</h4>
-                        <div className='wte__search-textbox'>
-                            <input type="text" id="locationName" name="locationName" placeholder=" " autocomplete="off" aria-labelledby="locationNamePlaceholder"/>
-                            <label className="wte__search-textbox-placeholder" for="locationName" id="locationNamePlaceholder">
-                                <div className="wte__search-textbox-str">Name of location</div>
+class Search extends React.Component{
+    render() {
+        return (
+            <form action="">
+                <div className='wte__search'>
+                    <div className='wte__search-container'>
+                        <div className='wte__search-content'>
+                            <h4>Location</h4>
+                            <div className='wte__search-textbox'>
+                                <input type="text" id="locationName" name="locationName" placeholder=" " autocomplete="off" aria-labelledby="locationNamePlaceholder"/>
+                                <label className="wte__search-textbox-placeholder" for="locationName" id="locationNamePlaceholder">
+                                    <div className="wte__search-textbox-str">Name of location</div>
+                                </label>
+                            </div>
+                            <label className='wte__search-checkbox'>Choose your current location
+                                <input type="checkbox" />
+                                <span className='wte__search-checkmark'></span>
+                            </label>
+                            <div className='wte__search-textbox wte__search-numberbox'>
+                                <label for="searchRadius">Search radius:</label>
+                                <input type="number" id="searchRadius" /><span>km</span>
+                            </div>
+                        </div>
+                        <div className='wte__search-content'>
+                            <h4>Food category</h4>
+                            <div className='wte__search-textbox'>
+                                <input type="text" id="foodCategory" name="foodCategory" placeholder=" " autocomplete="off" aria-labelledby='foodCategoryPlaceholdere'/>
+                                <label className="wte__search-textbox-placeholder" for="foodCategory" id="foodCategoryPlaceholder">
+                                    <div className='wte__search-textbox-str'>Food category you'd like</div>
+                                </label>
+                            </div>
+                            <label className='wte__search-checkbox'>Randomize the food category
+                                <input type="checkbox" />
+                                <span className='wte__search-checkmark'></span>
                             </label>
                         </div>
-                        <label className='wte__search-checkbox'>Choose your current location
-                            <input type="checkbox" />
-                            <span className='wte__search-checkmark'></span>
-                        </label>
-                        <div className='wte__search-textbox wte__search-numberbox'>
-                            <label for="searchRadius">Search radius:</label>
-                            <input type="number" id="searchRadius" /><span>km</span>
-                        </div>
                     </div>
-                    <div className='wte__search-content'>
-                        <h4>Food category</h4>
-                        <div className='wte__search-textbox'>
-                            <input type="text" id="foodCategory" name="foodCategory" placeholder=" " autocomplete="off" aria-labelledby='foodCategoryPlaceholdere'/>
-                            <label className="wte__search-textbox-placeholder" for="foodCategory" id="foodCategoryPlaceholder">
-                                <div className='wte__search-textbox-str'>Food category you'd like</div>
-                            </label>
-                        </div>
-                        <label className='wte__search-checkbox'>Randomize the food category
-                            <input type="checkbox" />
-                            <span className='wte__search-checkmark'></span>
-                        </label>
+                    <div className='wte__search-button'>
+                        <button type="submit">Search</button>
+                        <p>I'm hungry...</p>
                     </div>
                 </div>
-                <div className='wte__search-button'>
-                    <button type="submit">Search</button>
-                    <p>I'm hungry...</p>
-                </div>
-            </div>
-        </form>
-    )
+            </form>
+        )
+    }
 }
+
+// const Search = () => {
+//     return (
+//         <form action="">
+//             <div className='wte__search'>
+//                 <div className='wte__search-container'>
+//                     <div className='wte__search-content'>
+//                         <h4>Location</h4>
+//                         <div className='wte__search-textbox'>
+//                             <input type="text" id="locationName" name="locationName" placeholder=" " autocomplete="off" aria-labelledby="locationNamePlaceholder"/>
+//                             <label className="wte__search-textbox-placeholder" for="locationName" id="locationNamePlaceholder">
+//                                 <div className="wte__search-textbox-str">Name of location</div>
+//                             </label>
+//                         </div>
+//                         <label className='wte__search-checkbox'>Choose your current location
+//                             <input type="checkbox" />
+//                             <span className='wte__search-checkmark'></span>
+//                         </label>
+//                         <div className='wte__search-textbox wte__search-numberbox'>
+//                             <label for="searchRadius">Search radius:</label>
+//                             <input type="number" id="searchRadius" /><span>km</span>
+//                         </div>
+//                     </div>
+//                     <div className='wte__search-content'>
+//                         <h4>Food category</h4>
+//                         <div className='wte__search-textbox'>
+//                             <input type="text" id="foodCategory" name="foodCategory" placeholder=" " autocomplete="off" aria-labelledby='foodCategoryPlaceholdere'/>
+//                             <label className="wte__search-textbox-placeholder" for="foodCategory" id="foodCategoryPlaceholder">
+//                                 <div className='wte__search-textbox-str'>Food category you'd like</div>
+//                             </label>
+//                         </div>
+//                         <label className='wte__search-checkbox'>Randomize the food category
+//                             <input type="checkbox" />
+//                             <span className='wte__search-checkmark'></span>
+//                         </label>
+//                     </div>
+//                 </div>
+//                 <div className='wte__search-button'>
+//                     <button type="submit">Search</button>
+//                     <p>I'm hungry...</p>
+//                 </div>
+//             </div>
+//         </form>
+//     )
+// }
 
 export default Search
